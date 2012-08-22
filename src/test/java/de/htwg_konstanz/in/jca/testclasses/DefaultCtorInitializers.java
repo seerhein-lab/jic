e31a2ed3c0e1c1a5109cf1ef32b2a3496faaac11
StefanWaldraff <stefan.waldraff@gmail.com>
2012-08-22 23:23:47 +0200

    Added comment with informations for TestLocalVars.

--- src/test/java/de/htwg_konstanz/in/jca/testclasses/DefaultCtorInitializers.java
+++ src/test/java/de/htwg_konstanz/in/jca/testclasses/DefaultCtorInitializers.java
@@ -1,5 +1,17 @@
 package de.htwg_konstanz.in.jca.testclasses;
 
+/*for TestLocalVars:
+ *	private static final Class USED_CLASS = DefaultCtorInitializers.class;
+ *
+ *	private static void setExpectedData(){
+ *  	Entry[] entry1 = { Entry.thisReference, Entry.someByte, Entry.someShort, Entry.someInt, Entry.someLong,
+ *  Entry.someFloat, Entry.someDouble, Entry.someChar, Entry.someBoolean, Entry.someReference };
+ *		EXPECTED_ENTRIES.add(entry1);
+ *		int[] indexes1 = { 0, 1,2,3,4,6,7,9,10,11,12 };
+ *		EXPECTED:INDEXES.add(indexes1);
+ *	}
+ */
+
 public class DefaultCtorInitializers {
 
     final byte b = 7;
