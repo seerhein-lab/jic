6d3bb55f12854de40acbab5aa3b37ca7d95c6707
fischer <fischer@N-Diagnostix.seitenbau.net>
2012-08-24 10:08:04 +0200

    add javadoc

--- src/main/java/de/htwg_konstanz/in/jca/Entry.java
+++ src/main/java/de/htwg_konstanz/in/jca/Entry.java
@@ -1,7 +1,42 @@
 package de.htwg_konstanz.in.jca;
 
-public enum Entry { someByte, someShort, someInt, someLong, someFloat, someDouble, someChar, 
-    someBoolean, someReference, thisReference, unknownValue;
+/**
+ * An entry in a frame stack.
+ */
+public enum Entry {
+
+    /** A byte with an arbitrary value. */
+    someByte, 
+
+    /** A short with an arbitrary value. */
+    someShort,
+    
+    /** An int with an arbitrary value. */
+    someInt,
+    
+    /** A long with an arbitrary value. */
+    someLong, 
+
+    /** A float with an arbitrary value. */
+    someFloat,
+    
+    /** A double with an arbitrary value. */
+    someDouble, 
+    
+    /** A character with an arbitrary value. */
+    someChar, 
+
+    /** A boolean with an arbitrary value. */
+    someBoolean, 
+    
+    /** A reference to an arbitrary object, but not to the object to check. */
+    someReference,
+    
+    /** A reference to the object to check. */
+    thisReference, 
+    
+    /** An unknown value. */
+    unknownValue;
         	
     public static Entry getInstance(String signature) {
 	if ( signature.equals("I") )  return someInt;
