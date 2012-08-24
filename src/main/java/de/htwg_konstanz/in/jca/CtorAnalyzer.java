c5dedd80ac31ab484ea5354ca199a4c818d32d8a
fischer <fischer@N-Diagnostix.seitenbau.net>
2012-08-24 10:07:46 +0200

    add javadoc

--- src/main/java/de/htwg_konstanz/in/jca/CtorAnalyzer.java
+++ src/main/java/de/htwg_konstanz/in/jca/CtorAnalyzer.java
@@ -7,36 +7,67 @@ import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.Instruction;
 import org.apache.bcel.generic.InstructionList;
 
-
-public class CtorAnalyzer {    
+/**
+ * Analyzes constructors.
+ */
+public class CtorAnalyzer {
+	
+	/** The constructor to analyze. */
     private final Method ctor;
+
+    /** The result of the constructor call. */
     private Entry result;
     
-    public CtorAnalyzer(Method ctor) {
-	this.ctor = ctor;
-    }
-    
-    public Entry getResult() { return result; }
-    
-
-    
-    public ThreeValueBoolean doesThisReferenceEscape() {
-	Stack<Entry> callerStack = new Stack<Entry>();
-	
-	// push this + args onto the stack
-	callerStack.push(Entry.thisReference);
-	
-	LocalVariable[] localVars = ( ctor.getCode().getLocalVariableTable() == null ) ? 
-		new LocalVariable[0] : ctor.getCode().getLocalVariableTable().getLocalVariableTable();
-	
-	for ( int i = 1; i < localVars.length; i++ ) {
-	    callerStack.push(Entry.getInstance(localVars[i].getSignature()));
+    /**
+     * Constructor.
+     *
+     * @param ctor The constructor to analyze, not null.
+     */
+	public CtorAnalyzer(Method ctor) {
+		this.ctor = ctor;
 	}
-	
-	return doesThisReferenceEscape(callerStack);
-    }
-	
     
+	/**
+	 * Returns the result of the constructor call.
+	 * 
+	 * @return the result of the constructor call, or null
+	 *         if not yet determined.
+	 */
+	public Entry getResult() {
+		return result;
+	}    
+
+    /**
+     * Checks whether the reference of the checked object is passed to another
+     * object in the constructor.
+     *  
+     * @return whether the reference of the checked object escapes.
+     */
+	public ThreeValueBoolean doesThisReferenceEscape() {
+		Stack<Entry> callerStack = new Stack<Entry>();
+		
+		// push this + args onto the stack
+		callerStack.push(Entry.thisReference);
+		
+		LocalVariable[] localVars = ( ctor.getCode().getLocalVariableTable() == null ) ? 
+				new LocalVariable[0] : ctor.getCode().getLocalVariableTable().getLocalVariableTable();
+		
+		for ( int i = 1; i < localVars.length; i++ ) {
+		    callerStack.push(Entry.getInstance(localVars[i].getSignature()));
+		}
+		
+		return doesThisReferenceEscape(callerStack);
+	}	
+
+    /**
+     * Checks whether the reference of the checked object is passed to another
+     * object in the constructor.
+     *  
+     * @param callerStack the content of the local variable table 
+     *        of the constructor.
+     *
+     * @return whether the reference of the checked object escapes.
+     */
     public ThreeValueBoolean doesThisReferenceEscape(Stack<Entry> callerStack) {	
 	LocalVars localVars = new LocalVars((ctor.getLocalVariableTable() == null ) ? 
 		new LocalVariable[0] : ctor.getLocalVariableTable().getLocalVariableTable());
