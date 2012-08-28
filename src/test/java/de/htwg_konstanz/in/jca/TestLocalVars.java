f39a1b3e28512404c3cf8aa86fc4bf4eb5ed4d9b
StefanWaldraff <stefan.waldraff@gmail.com>
2012-08-28 11:21:59 +0200

    Changes on test methods.
    - removed Strings form assertEquals()
    - merged testLocalVars(), testLocalVarsEntries() and
    testLocalVarsIndexes() to testLocalVars()
    - renamed testGetForIndexError() to testGetForIndexOutOfBoundsError()
    - renamed testSetForIndexError() to testSetForIndexOutOfBoundsError()
    - added testGetForIndexWithinBoundsError()
    - added testSetForIndexWithinBoundsError()

--- src/test/java/de/htwg_konstanz/in/jca/TestLocalVars.java
+++ src/test/java/de/htwg_konstanz/in/jca/TestLocalVars.java
@@ -13,6 +13,9 @@ import org.junit.Test;
 
 import de.htwg_konstanz.in.jca.testclasses.ClassWithAllTypes;
 
+/**
+ * JUnit test class for LocalVars.
+ */
 public class TestLocalVars {
 
 	private LocalVariable[] localVariableTableExample;
@@ -47,72 +50,43 @@ public class TestLocalVars {
 	@Test
 	public void testLocalVarsZeroLength() {
 		LocalVars localVars = new LocalVars(new LocalVariable[0]);
-		assertEquals("entries.length should be 0.", 0,
-				localVars.getEntriesLength());
-		assertEquals("indexes.length should be 0.", 0,
-				localVars.getIndexesLength());
+		assertEquals(0, localVars.getEntriesLength());
+		assertEquals(0, localVars.getIndexesLength());
 	}
 
 	/**
-	 * Tests the constructor with an example VariableTable array.
+	 * Tests the constructor with an example VariableTable array. Therefore the
+	 * arrays must have the right length and the values must be validated.
 	 */
 	@Test
 	public void testLocalVars() {
 		LocalVars localVars = new LocalVars(localVariableTableExample);
-		assertEquals(
-				"localVars should contain 10 entries (1 for thisReference, 9 for other variables).",
-				10, localVars.getEntriesLength());
-		assertEquals(
-				"localVars should contain 10 indexes (1 for thisReference, 9 for other variables).",
-				10, localVars.getIndexesLength());
-		assertEquals("Each entry should have an index.",
-				localVars.getEntriesLength(), localVars.getIndexesLength());
-	}
-
-	/**
-	 * Tests if LocalVars sets the right entries.
-	 */
-	@Test
-	public void testLocalVarsEntries() {
-		LocalVars localVars = new LocalVars(localVariableTableExample);
-		assertEquals("1st entry should be someReference.", Entry.someReference,
-				localVars.getEntry(0));
-		assertEquals("2nd entry should be someByte.", Entry.someByte,
-				localVars.getEntry(1));
-		assertEquals("3th entry should be someShort.", Entry.someShort,
-				localVars.getEntry(2));
-		assertEquals("4th entry should be someReference.", Entry.someReference,
-				localVars.getEntry(3));
-		assertEquals("5th entry should be someInt.", Entry.someInt,
-				localVars.getEntry(4));
-		assertEquals("6th entry should be someLong.", Entry.someLong,
-				localVars.getEntry(5));
-		assertEquals("7th entry should be someFloat.", Entry.someFloat,
-				localVars.getEntry(6));
-		assertEquals("8th entry should be someDouble.", Entry.someDouble,
-				localVars.getEntry(7));
-		assertEquals("9th entry should be someChar.", Entry.someChar,
-				localVars.getEntry(8));
-		assertEquals("10th entry should be someBoolean.", Entry.someBoolean,
-				localVars.getEntry(9));
-	}
-
-	/**
-	 * Tests if LocalVars sets the right indexes.
-	 */
-	@Test
-	public void testLocalVarsIndexes() {
-		LocalVars localVars = new LocalVars(localVariableTableExample);
-		assertEquals("1st index should be 0.", 0, localVars.getIndex(0));
-		assertEquals("2nd index should be 1.", 1, localVars.getIndex(1));
-		assertEquals("3th index should be 2.", 2, localVars.getIndex(2));
-		assertEquals("4th index should be 3.", 3, localVars.getIndex(3));
-		assertEquals("5th index should be 4.", 4, localVars.getIndex(4));
-		assertEquals("6th index should be 5.", 5, localVars.getIndex(5));
-		assertEquals("7th index should be 7.", 7, localVars.getIndex(6));
-		assertEquals("8th index should be 8.", 8, localVars.getIndex(7));
-		assertEquals("9th index should be 10.", 10, localVars.getIndex(8));
-		assertEquals("10th index should be 11.", 11, localVars.getIndex(9));
+		// checks lengths
+		assertEquals(10, localVars.getEntriesLength());
+		assertEquals(10, localVars.getIndexesLength());
+		assertEquals(localVars.getEntriesLength(), localVars.getIndexesLength());
+		// checks entries
+		assertEquals(Entry.someReference, localVars.getEntry(0));
+		assertEquals(Entry.someByte, localVars.getEntry(1));
+		assertEquals(Entry.someShort, localVars.getEntry(2));
+		assertEquals(Entry.someReference, localVars.getEntry(3));
+		assertEquals(Entry.someInt, localVars.getEntry(4));
+		assertEquals(Entry.someLong, localVars.getEntry(5));
+		assertEquals(Entry.someFloat, localVars.getEntry(6));
+		assertEquals(Entry.someDouble, localVars.getEntry(7));
+		assertEquals(Entry.someChar, localVars.getEntry(8));
+		assertEquals(Entry.someBoolean, localVars.getEntry(9));
+		// checks indexes
+		assertEquals(0, localVars.getIndex(0));
+		assertEquals(1, localVars.getIndex(1));
+		assertEquals(2, localVars.getIndex(2));
+		assertEquals(3, localVars.getIndex(3));
+		assertEquals(4, localVars.getIndex(4));
+		assertEquals(5, localVars.getIndex(5));
+		assertEquals(7, localVars.getIndex(6));
+		assertEquals(8, localVars.getIndex(7));
+		assertEquals(10, localVars.getIndex(8));
+		assertEquals(11, localVars.getIndex(9));
 	}
 
 	/**
@@ -124,11 +98,8 @@ public class TestLocalVars {
 		LocalVars localVars = new LocalVars(new LocalVariable[0]);
 		Stack<Entry> callerStack = new Stack<Entry>();
 		callerStack.add(Entry.someReference);
-		assertEquals("callerStack should not be empty.", true,
-				!callerStack.isEmpty());
 		localVars.initWithArgs(callerStack, 1);
-		assertEquals("callerStack should be emptied.", true,
-				callerStack.isEmpty());
+		assertEquals(true, callerStack.isEmpty());
 	}
 
 	/**
@@ -154,26 +125,17 @@ public class TestLocalVars {
 		Stack<Entry> callerStack = new Stack<Entry>();
 		callerStack.add(Entry.someFloat);
 		localVars.initWithArgs(callerStack, 0);
-		assertEquals("1st entry should be someReference.", Entry.someReference,
-				localVars.getEntry(0));
-		assertEquals("2nd entry should be someByte.", Entry.someByte,
-				localVars.getEntry(1));
-		assertEquals("3th entry should be someShort.", Entry.someShort,
-				localVars.getEntry(2));
-		assertEquals("4th entry should be someReference.", Entry.someReference,
-				localVars.getEntry(3));
-		assertEquals("5th entry should be someInt.", Entry.someInt,
-				localVars.getEntry(4));
-		assertEquals("6th entry should be someLong.", Entry.someLong,
-				localVars.getEntry(5));
-		assertEquals("7th entry should be someFloat.", Entry.someFloat,
-				localVars.getEntry(6));
-		assertEquals("8th entry should be someDouble.", Entry.someDouble,
-				localVars.getEntry(7));
-		assertEquals("9th entry should be someChar.", Entry.someChar,
-				localVars.getEntry(8));
-		assertEquals("10th entry should be someBoolean.", Entry.someBoolean,
-				localVars.getEntry(9));
+		assertEquals(true, !callerStack.isEmpty());
+		assertEquals(Entry.someReference, localVars.getEntry(0));
+		assertEquals(Entry.someByte, localVars.getEntry(1));
+		assertEquals(Entry.someShort, localVars.getEntry(2));
+		assertEquals(Entry.someReference, localVars.getEntry(3));
+		assertEquals(Entry.someInt, localVars.getEntry(4));
+		assertEquals(Entry.someLong, localVars.getEntry(5));
+		assertEquals(Entry.someFloat, localVars.getEntry(6));
+		assertEquals(Entry.someDouble, localVars.getEntry(7));
+		assertEquals(Entry.someChar, localVars.getEntry(8));
+		assertEquals(Entry.someBoolean, localVars.getEntry(9));
 	}
 
 	/**
@@ -189,19 +151,13 @@ public class TestLocalVars {
 		}
 		localVars.initWithArgs(callerStack, 5);
 		for (int i = 0; i < 5; i++) {
-			assertEquals("Entry " + (i + 1) + " should be thisReference.",
-					Entry.thisReference, localVars.getEntry(i));
+			assertEquals(Entry.thisReference, localVars.getEntry(i));
 		}
-		assertEquals("Entry 6 should be someLong.", Entry.someLong,
-				localVars.getEntry(5));
-		assertEquals("Entry 7 should be someFloat.", Entry.someFloat,
-				localVars.getEntry(6));
-		assertEquals("Entry 8 should be someDouble.", Entry.someDouble,
-				localVars.getEntry(7));
-		assertEquals("Entry 9 should be someChar.", Entry.someChar,
-				localVars.getEntry(8));
-		assertEquals("Entry 10 should be someBoolean.", Entry.someBoolean,
-				localVars.getEntry(9));
+		assertEquals(Entry.someLong, localVars.getEntry(5));
+		assertEquals(Entry.someFloat, localVars.getEntry(6));
+		assertEquals(Entry.someDouble, localVars.getEntry(7));
+		assertEquals(Entry.someChar, localVars.getEntry(8));
+		assertEquals(Entry.someBoolean, localVars.getEntry(9));
 	}
 
 	/**
@@ -217,8 +173,7 @@ public class TestLocalVars {
 		}
 		localVars.initWithArgs(callerStack, callerStack.size());
 		for (int i = 0; i < localVars.getEntriesLength(); i++) {
-			assertEquals("Entry" + (i + 1) + " should be thisReference.",
-					Entry.thisReference, localVars.getEntry(i));
+			assertEquals(Entry.thisReference, localVars.getEntry(i));
 		}
 	}
 
@@ -238,13 +193,25 @@ public class TestLocalVars {
 	}
 
 	/**
-	 * Tests if getForIndex() throws AssertionError
+	 * Tests if getForIndex() throws AssertionError when index is out of bounds.
 	 */
 	@Test(expected = AssertionError.class)
-	public void testGetForIndexError() {
+	public void testGetForIndexOutOfBounds() {
 		// creates example LocalVars with 10 entries
 		LocalVars localVars = new LocalVars(localVariableTableExample);
 		localVars.getForIndex(-1);
+	}
+
+	/**
+	 * Tests if getForIndex() throws AssertionError when index is within the
+	 * bounds but invalid.
+	 */
+	@Test(expected = AssertionError.class)
+	public void testGetForIndexWithinBoundsError() {
+		// creates example LocalVars with 10 entries
+		LocalVars localVars = new LocalVars(localVariableTableExample);
+		// tries to access index 6 (the long starts at 5)
+		localVars.getForIndex(6);
 	}
 
 	/**
@@ -254,36 +221,37 @@ public class TestLocalVars {
 	public void testGetForIndex() {
 		// creates example LocalVars with 10 entries
 		LocalVars localVars = new LocalVars(localVariableTableExample);
-		assertEquals("1st entry should be someReference.", Entry.someReference,
-				localVars.getForIndex(0));
-		assertEquals("2nd entry should be someByte.", Entry.someByte,
-				localVars.getForIndex(1));
-		assertEquals("3th entry should be someShort.", Entry.someShort,
-				localVars.getForIndex(2));
-		assertEquals("4th entry should be someReference.", Entry.someReference,
-				localVars.getForIndex(3));
-		assertEquals("5th entry should be someInt.", Entry.someInt,
-				localVars.getForIndex(4));
-		assertEquals("6th entry should be someLong.", Entry.someLong,
-				localVars.getForIndex(5));
-		assertEquals("7th entry should be someFloat.", Entry.someFloat,
-				localVars.getForIndex(7));
-		assertEquals("8th entry should be someDouble.", Entry.someDouble,
-				localVars.getForIndex(8));
-		assertEquals("9th entry should be someChar.", Entry.someChar,
-				localVars.getForIndex(10));
-		assertEquals("10th entry should be someBoolean.", Entry.someBoolean,
-				localVars.getForIndex(11));
+		assertEquals(Entry.someReference, localVars.getForIndex(0));
+		assertEquals(Entry.someByte, localVars.getForIndex(1));
+		assertEquals(Entry.someShort, localVars.getForIndex(2));
+		assertEquals(Entry.someReference, localVars.getForIndex(3));
+		assertEquals(Entry.someInt, localVars.getForIndex(4));
+		assertEquals(Entry.someLong, localVars.getForIndex(5));
+		assertEquals(Entry.someFloat, localVars.getForIndex(7));
+		assertEquals(Entry.someDouble, localVars.getForIndex(8));
+		assertEquals(Entry.someChar, localVars.getForIndex(10));
+		assertEquals(Entry.someBoolean, localVars.getForIndex(11));
 	}
 
 	/**
-	 * Tests if setForIndex() throws AssertionError.
+	 * Tests if setForIndex() throws AssertionError when index is out of bounds.
 	 */
 	@Test(expected = AssertionError.class)
-	public void testSetForIndexError() {
+	public void testSetForIndexOutOfBoundsError() {
 		// creates example LocalVars with 10 entries
 		LocalVars localVars = new LocalVars(localVariableTableExample);
 		localVars.setForIndex(-1, Entry.someReference);
+	}
+
+	/**
+	 * Tests if setForIndex() throws AssertionError when index is within bounds.
+	 */
+	@Test(expected = AssertionError.class)
+	public void testSetForIndexWithinBoundsError() {
+		// creates example LocalVars with 10 entries
+		LocalVars localVars = new LocalVars(localVariableTableExample);
+		// tries to access index 9 (the double starts at 8)
+		localVars.setForIndex(9, Entry.someReference);
 	}
 
 	/**
@@ -303,26 +271,16 @@ public class TestLocalVars {
 		localVars.setForIndex(8, Entry.someShort);
 		localVars.setForIndex(10, Entry.someByte);
 		localVars.setForIndex(11, Entry.thisReference);
-		assertEquals("1st entry should be someBoolean.", Entry.someBoolean,
-				localVars.getEntry(0));
-		assertEquals("2nd entry should be someChar.", Entry.someChar,
-				localVars.getEntry(1));
-		assertEquals("3th entry should be someDouble.", Entry.someDouble,
-				localVars.getEntry(2));
-		assertEquals("4th entry should be someFloat.", Entry.someFloat,
-				localVars.getEntry(3));
-		assertEquals("5th entry should be someLong.", Entry.someLong,
-				localVars.getEntry(4));
-		assertEquals("6th entry should be someInt.", Entry.someInt,
-				localVars.getEntry(5));
-		assertEquals("7th entry should be someReference.", Entry.someReference,
-				localVars.getEntry(6));
-		assertEquals("8th entry should be someShort.", Entry.someShort,
-				localVars.getEntry(7));
-		assertEquals("9th entry should be someByte.", Entry.someByte,
-				localVars.getEntry(8));
-		assertEquals("10th entry should be thisReference.",
-				Entry.thisReference, localVars.getEntry(9));
+		assertEquals(Entry.someBoolean, localVars.getEntry(0));
+		assertEquals(Entry.someChar, localVars.getEntry(1));
+		assertEquals(Entry.someDouble, localVars.getEntry(2));
+		assertEquals(Entry.someFloat, localVars.getEntry(3));
+		assertEquals(Entry.someLong, localVars.getEntry(4));
+		assertEquals(Entry.someInt, localVars.getEntry(5));
+		assertEquals(Entry.someReference, localVars.getEntry(6));
+		assertEquals(Entry.someShort, localVars.getEntry(7));
+		assertEquals(Entry.someByte, localVars.getEntry(8));
+		assertEquals(Entry.thisReference, localVars.getEntry(9));
 	}
 
 }
