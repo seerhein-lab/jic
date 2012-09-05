9753ced6b4c2082b0244f9298c97eae2d9219548
StefanWaldraff <stefan.waldraff@gmail.com>
2012-09-05 10:42:21 +0200

    Changed try-catch-blocks to throws ClassNotFoundException.

--- src/test/java/de/htwg_konstanz/in/jca/TestClassAnalyzer.java
+++ src/test/java/de/htwg_konstanz/in/jca/TestClassAnalyzer.java
@@ -1,7 +1,6 @@
 package de.htwg_konstanz.in.jca;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
@@ -17,80 +16,61 @@ import de.htwg_konstanz.in.jca.testclasses.DefaultCtorNoInitializers;
 public class TestClassAnalyzer {
 
 	@Test
-	public void testProperlyConstructedDefaultCtorNoInitializers() {
-		JavaClass clazz = null;
-		try {
-			clazz = Repository.lookupClass(DefaultCtorNoInitializers.class
-					.getCanonicalName());
-		} catch (ClassNotFoundException e) {
-			fail("class not found");
-		}
+	public void testProperlyConstructedDefaultCtorNoInitializers()
+			throws ClassNotFoundException {
+		JavaClass clazz = Repository
+				.lookupClass(DefaultCtorNoInitializers.class.getCanonicalName());
+
 		assertEquals(ClassAnalyzer.indicatesSuccess(new ClassAnalyzer(clazz)
 				.properlyConstructed()), ThreeValueBoolean.yes);
 	}
 
 	@Test
-	public void testProperlyConstructedDefaultCtorInitializersByte() {
-		JavaClass clazz = null;
-		try {
-			clazz = Repository.lookupClass(DefaultCtorInitializersByte.class
-					.getCanonicalName());
-		} catch (ClassNotFoundException e) {
-			fail("class not found");
-		}
+	public void testProperlyConstructedDefaultCtorInitializersByte()
+			throws ClassNotFoundException {
+		JavaClass clazz = Repository
+				.lookupClass(DefaultCtorInitializersByte.class
+						.getCanonicalName());
 		assertEquals(ClassAnalyzer.indicatesSuccess(new ClassAnalyzer(clazz)
 				.properlyConstructed()), ThreeValueBoolean.yes);
 	}
 
 	@Test
-	public void testProperlyConstructedDefaultCtorInitializersChar() {
-		JavaClass clazz = null;
-		try {
-			clazz = Repository.lookupClass(DefaultCtorInitializersChar.class
-					.getCanonicalName());
-		} catch (ClassNotFoundException e) {
-			fail("class not found");
-		}
+	public void testProperlyConstructedDefaultCtorInitializersChar()
+			throws ClassNotFoundException {
+		JavaClass clazz = Repository
+				.lookupClass(DefaultCtorInitializersChar.class
+						.getCanonicalName());
 		assertEquals(ClassAnalyzer.indicatesSuccess(new ClassAnalyzer(clazz)
 				.properlyConstructed()), ThreeValueBoolean.yes);
 	}
 
 	@Test
-	public void testProperlyConstructedDefaultCtorInitializersLong() {
-		JavaClass clazz = null;
-		try {
-			clazz = Repository.lookupClass(DefaultCtorInitializersLong.class
-					.getCanonicalName());
-		} catch (ClassNotFoundException e) {
-			fail("class not found");
-		}
+	public void testProperlyConstructedDefaultCtorInitializersLong()
+			throws ClassNotFoundException {
+		JavaClass clazz = Repository
+				.lookupClass(DefaultCtorInitializersLong.class
+						.getCanonicalName());
 		assertEquals(ClassAnalyzer.indicatesSuccess(new ClassAnalyzer(clazz)
 				.properlyConstructed()), ThreeValueBoolean.yes);
 	}
 
 	@Test
-	public void testProperlyConstructedDefaultCtorInitializersObject() {
-		JavaClass clazz = null;
-		try {
-			clazz = Repository.lookupClass(DefaultCtorInitializersObject.class
-					.getCanonicalName());
-		} catch (ClassNotFoundException e) {
-			fail("class not found");
-		}
+	public void testProperlyConstructedDefaultCtorInitializersObject()
+			throws ClassNotFoundException {
+		JavaClass clazz = Repository
+				.lookupClass(DefaultCtorInitializersObject.class
+						.getCanonicalName());
 		assertEquals(ClassAnalyzer.indicatesSuccess(new ClassAnalyzer(clazz)
 				.properlyConstructed()), ThreeValueBoolean.yes);
 	}
 
 	@Test
-	public void testProperlyConstructedDefaultCtorInitializersLongObject() {
-		JavaClass clazz = null;
-		try {
-			clazz = Repository
-					.lookupClass(DefaultCtorInitializersLongObject.class
-							.getCanonicalName());
-		} catch (ClassNotFoundException e) {
-			fail("class not found");
-		}
+	public void testProperlyConstructedDefaultCtorInitializersLongObject()
+			throws ClassNotFoundException {
+		JavaClass clazz = Repository
+				.lookupClass(DefaultCtorInitializersLongObject.class
+						.getCanonicalName());
 		assertEquals(ClassAnalyzer.indicatesSuccess(new ClassAnalyzer(clazz)
 				.properlyConstructed()), ThreeValueBoolean.yes);
 	}
