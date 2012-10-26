fed6c53c50b9506f251d8ecc67c49b8512a5dc4f
StefanWaldraff <stefan.waldraff@gmail.com>
2012-10-26 23:24:27 +0200

    implemented visitGETSTATIC and visitPUTSTATIC #7

--- src/main/java/playground/PropConstTestClass.java
+++ src/main/java/playground/PropConstTestClass.java
@@ -2,13 +2,10 @@ package playground;
 
 public class PropConstTestClass {
 
-	public static double f(int i, double d) {
-		return d;
-	}
+	public static Object x;
 
 	public PropConstTestClass() {
-		int i = 4711;
-		double d = f(i, 3.14);
-
+		x = this;
+		Object i = x;
 	}
 }
