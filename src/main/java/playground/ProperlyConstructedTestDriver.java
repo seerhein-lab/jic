0bcf08edc024b062bec78ae0d2282f8dbf613612
StefanWaldraff <stefan.waldraff@gmail.com>
2012-11-14 14:01:34 +0100

    Started to convert sysos to logger (Part 2).

--- src/main/java/playground/ProperlyConstructedTestDriver.java
+++ src/main/java/playground/ProperlyConstructedTestDriver.java
@@ -1,5 +1,12 @@
 package playground;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
 
@@ -10,16 +17,40 @@ import edu.umd.cs.findbugs.BugInstance;
 public class ProperlyConstructedTestDriver {
 
 	public static void main(String[] args) throws ClassNotFoundException {
+		// logging stuff
+		Logger globalLogger = Logger.getLogger("");
+		Handler[] globalLoggerHandlers = globalLogger.getHandlers();
+		for (Handler handler : globalLoggerHandlers) {
+			globalLogger.removeHandler(handler);
+		}
+		ConsoleHandler consoleHandler = new ConsoleHandler();
+		consoleHandler
+				.setFormatter(new ProperlyConstructedTestDriver().new ProperlyConstructedTestDriverFormater());
+		globalLogger.addHandler(consoleHandler);
+		globalLogger.setLevel(Level.ALL);
+		Logger logger = Logger.getLogger("ProperlyConstructedTestDriver");
+		// end logging set up
+
 		JavaClass clazz = Repository
 				.lookupClass("playground.PropConstTestClass");
 
 		BugCollection bugs = new PropConClassAnalyzer(clazz)
 				.properlyConstructed();
-		System.out.println("bugs: ");
+		logger.log(Level.SEVERE, "bugs: ");
 		for (BugInstance bug : bugs) {
-			System.out.println("  " + bug);
+			logger.log(Level.SEVERE, "  " + bug);
 		}
-		System.out.println("end bugs");
+
+		logger.log(Level.SEVERE, "end bugs");
+
+	}
+
+	public class ProperlyConstructedTestDriverFormater extends Formatter {
+
+		@Override
+		public String format(LogRecord arg0) {
+			return arg0.getMessage() + "\n";
+		}
 
 	}
 
