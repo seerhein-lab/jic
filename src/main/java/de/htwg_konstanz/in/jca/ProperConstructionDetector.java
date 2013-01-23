d9be0cc31189e90f33315b5bc929ef3ddc16ad21
Oliver Haase <haase@htwg-konstanz.de>
2013-01-23 14:11:01 +0100

    Detector class for Findbugs plugin

---
+++ src/main/java/de/htwg_konstanz/in/jca/ProperConstructionDetector.java
@@ -0,0 +1,44 @@
+package de.htwg_konstanz.in.jca;
+
+import org.apache.bcel.classfile.JavaClass;
+
+import edu.umd.cs.findbugs.BugCollection;
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.Detector;
+import edu.umd.cs.findbugs.ba.ClassContext;
+
+public final class ProperConstructionDetector implements Detector {
+	private BugReporter reporter;
+
+	public ProperConstructionDetector(BugReporter reporter) {
+		this.reporter = reporter;
+	}
+
+	@Override
+	public void report() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		JavaClass clazz = classContext.getJavaClass();
+		System.out.println("**************************");
+		System.out.println("clazz: " + clazz);
+		System.out.println("**************************");
+
+		// reporter.reportBug(new BugInstance("TUTORIAL_BUG",
+		// Priorities.NORMAL_PRIORITY)
+		// .addClass("de.htwg_konstanz.in.jca.testclasses.UtilsTestClass"));
+
+		BugCollection bugs = new PropConClassAnalyzer(clazz, classContext)
+				.properlyConstructed();
+
+		for (BugInstance bug : bugs) {
+			System.out.println("bug" + bug);
+			reporter.reportBug(bug);
+		}
+
+	}
+}
