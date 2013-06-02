ddb13bcd8df663dad2d587d7a227e671ed457de9
bestehle <bestehle@htwg-konstanz.de>
2013-06-02 20:57:00 +0200

    added analyze() for FieldsNotModifiedMethodAnalyzer and FieldsNotPublishedMethodAnalyzer

--- src/main/java/de/seerhein_lab/jca/analyzer/fieldsNotPublishedAnalyzer/FieldsNotPublishedMethodAnalyzer.java
+++ src/main/java/de/seerhein_lab/jca/analyzer/fieldsNotPublishedAnalyzer/FieldsNotPublishedMethodAnalyzer.java
@@ -1,16 +1,20 @@
 package de.seerhein_lab.jca.analyzer.fieldsNotPublishedAnalyzer;
 
 import java.util.Set;
+import java.util.Stack;
 
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.InstructionHandle;
 import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.Type;
 
 import de.seerhein_lab.jca.AlreadyVisited;
 import de.seerhein_lab.jca.Frame;
 import de.seerhein_lab.jca.analyzer.BaseInstructionsAnalysisVisitor;
 import de.seerhein_lab.jca.analyzer.BaseMethodAnalyzer;
+import de.seerhein_lab.jca.heap.Heap;
+import de.seerhein_lab.jca.slot.ReferenceSlot;
 import de.seerhein_lab.jca.slot.Slot;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
@@ -36,23 +40,33 @@ public class FieldsNotPublishedMethodAnalyzer extends BaseMethodAnalyzer {
 	}
 
 	public void analyze() {
-		System.err.println("analyze not implemented yet!");
-		// TODO remove comment add specific "field" marker to all external
-		// references
-		// Stack<SlotOld> callerStack = new Stack<SlotOld>();
-		//
-		// // push this + args onto the stack
-		// callerStack.push(SlotOld.thisReference);
-		//
-		// Type[] argTypes = method.getArgumentTypes();
-		//
-		// for (Type argType : argTypes) {
-		// DataType dataType = DataType.getDataType(argType);
-		// for (int i = 0; i < dataType.getNumSlots(); i++) {
-		// callerStack.push(SlotOld.getDefaultInstance(dataType));
-		// }
-		//
-		// }
-		// analyze(callerStack);
+		Stack<Slot> callerStack = new Stack<Slot>();
+		Heap callerHeap = new Heap();
+
+		// push args + this (if not static) onto the stack
+		if (!method.isStatic()) {
+			ReferenceSlot thisReference = new ReferenceSlot(
+					callerHeap.getThisInstance());
+			callerStack.push(thisReference);
+		}
+
+		ReferenceSlot externalReference = new ReferenceSlot(
+				callerHeap.getExternalObject());
+
+		Type[] argTypes = method.getArgumentTypes();
+
+		for (Type argType : argTypes) {
+			Slot argument = Slot.getDefaultSlotInstance(argType);
+			if (argument instanceof ReferenceSlot) {
+				argument = externalReference;
+			}
+			for (int i = 0; i < argument.getNumSlots(); i++) {
+				callerStack.push(argument);
+			}
+		}
+
+		Frame callerFrame = new Frame(callerStack, callerHeap);
+
+		analyze(callerFrame);
 	}
 }
