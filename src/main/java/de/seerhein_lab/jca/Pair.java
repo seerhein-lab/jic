e8931106a5b114588959eb314200e899d41220c7
Oliver Haase <haase@htwg-konstanz.de>
2013-06-28 11:46:13 +0200

    Check for non-null components added, hashCode() and equals() adapted accordingly.

--- src/main/java/de/seerhein_lab/jca/Pair.java
+++ src/main/java/de/seerhein_lab/jca/Pair.java
@@ -5,6 +5,9 @@ public class Pair<T, K> {
 	private K value2;
 
 	public Pair(T value1, K value2) {
+		if ( value1 == null || value2 == null) 
+			throw new NullPointerException("Pair components must not be null");
+		
 		this.value1 = value1;
 		this.value2 = value2;
 	}
@@ -13,8 +16,8 @@ public class Pair<T, K> {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((value1 == null) ? 0 : value1.hashCode());
-		result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
+		result = prime * result + value1.hashCode();
+		result = prime * result + value2.hashCode();
 		return result;
 	}
 
@@ -23,28 +26,13 @@ public class Pair<T, K> {
 		if (this == obj) {
 			return true;
 		}
-		if (obj == null) {
-			return false;
-		}
+
 		if (!(obj instanceof Pair<?, ?>)) {
 			return false;
 		}
 		Pair<?, ?> other = (Pair<?, ?>) obj;
-		if (value1 == null) {
-			if (other.value1 != null) {
-				return false;
-			}
-		} else if (!value1.equals(other.value1)) {
-			return false;
-		}
-		if (value2 == null) {
-			if (other.value2 != null) {
-				return false;
-			}
-		} else if (!value2.equals(other.value2)) {
-			return false;
-		}
-		return true;
+		
+		return value1.equals(other.value1) && value2.equals(other.value2);
 	}
 
 }
