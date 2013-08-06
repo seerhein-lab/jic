90e07b4170404289d286009a6fc1310cfe58a60c
Oliver Haase <haase@htwg-konstanz.de>
2013-08-06 11:51:09 +0200

    Starting form here, the findbugs plugin now is named jca<version>.jar and executes isImmutable() rather than properlyConstructed(). No check for @Immutable annotation yet.

---
+++ src/main/java/de/seerhein_lab/jca/JcaDetector.java
@@ -0,0 +1,37 @@
+package de.seerhein_lab.jca;
+
+import org.apache.bcel.classfile.JavaClass;
+
+import de.seerhein_lab.jca.analyzer.ClassAnalyzer;
+import edu.umd.cs.findbugs.BugCollection;
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.Detector;
+import edu.umd.cs.findbugs.ba.ClassContext;
+
+public final class JcaDetector implements Detector {
+	private BugReporter reporter;
+
+	public JcaDetector(BugReporter reporter) {
+		this.reporter = reporter;
+	}
+
+	@Override
+	public void report() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		JavaClass clazz = classContext.getJavaClass();
+
+		BugCollection bugs = new ClassAnalyzer(clazz, classContext).isImmutable();
+
+		for (BugInstance bug : bugs) {
+			System.out.println("bug: " + bug);
+			reporter.reportBug(bug);
+		}
+
+	}
+}
