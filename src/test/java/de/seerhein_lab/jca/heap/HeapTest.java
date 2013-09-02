10c433d10e49faa23ced35705d43578c8e31ff51
bestehle <bestehle@htwg-konstanz.de>
2013-09-02 11:45:18 +0200

    added test for getReferredIterator to HeapTest

--- src/test/java/de/seerhein_lab/jca/heap/HeapTest.java
+++ src/test/java/de/seerhein_lab/jca/heap/HeapTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,15 +21,8 @@ public class HeapTest {
 	private Array f;
 
 	@Before
-	public void setUp() {
-		heap = new Heap();
-
-		a = heap.newClassInstance();
-		b = heap.newClassInstance();
-		c = heap.newClassInstance();
-		d = heap.newClassInstance();
-		e = heap.newClassInstance();
-		f = heap.newArray();
+	public void setUpReferences() {
+		setUpHeap();
 
 		a.setField("f", b);
 		b.setField("f", c);
@@ -119,9 +115,98 @@ public class HeapTest {
 		assertEquals(heap.getExternalObject(), heap.get(f.getId()));
 	}
 
-	// @Test
-	// public void testCopyConstructor() {
-	// Heap heapCopy = new Heap(heap);
-	// assertEquals(heap, heapCopy);
-	// }
+	@Test
+	public void testGetReferredIterator() {
+		setUpHeap();
+
+		a.setField("f0", b);
+		a.setField("f1", null);
+		a.setField("f2", null);
+		a.setField("f3", null);
+		a.setField("f4", c);
+		a.setField("f5", d);
+
+		HashSet<HeapObject> referredObjects = new HashSet<HeapObject>();
+		referredObjects.add(b);
+		referredObjects.add(c);
+		referredObjects.add(d);
+
+		checkReferredIterator(referredObjects);
+
+	}
+
+	@Test
+	public void testGetReferredIterator2() {
+		setUpHeap();
+
+		a.setField("f0", null);
+		a.setField("f1", null);
+		a.setField("f2", null);
+		a.setField("f3", null);
+		a.setField("f4", null);
+		a.setField("f5", null);
+
+		HashSet<HeapObject> referredObjects = new HashSet<HeapObject>();
+
+		checkReferredIterator(referredObjects);
+
+	}
+
+	@Test
+	public void testGetReferredIterator3() {
+		setUpHeap();
+
+		a.setField("f0", null);
+		a.setField("f1", null);
+		a.setField("f2", null);
+		a.setField("f3", null);
+		a.setField("f4", null);
+		a.setField("f5", b);
+
+		HashSet<HeapObject> referredObjects = new HashSet<HeapObject>();
+		referredObjects.add(b);
+
+		checkReferredIterator(referredObjects);
+
+	}
+
+	@Test
+	public void testGetReferredIterator4() {
+		setUpHeap();
+
+		a.setField("f0", b);
+		a.setField("f1", null);
+		a.setField("f2", null);
+		a.setField("f3", null);
+		a.setField("f4", null);
+		a.setField("f5", null);
+
+		HashSet<HeapObject> referredObjects = new HashSet<HeapObject>();
+		referredObjects.add(b);
+
+		checkReferredIterator(referredObjects);
+
+	}
+
+	public void checkReferredIterator(HashSet<HeapObject> referredObjects) {
+
+		Iterator<HeapObject> referredIterator = a.getReferredIterator();
+
+		while (referredIterator.hasNext()) {
+			assertTrue(referredObjects.contains(referredIterator.next()));
+		}
+
+		assertFalse(referredIterator.hasNext());
+	}
+
+	public void setUpHeap() {
+		heap = new Heap();
+
+		a = heap.newClassInstance();
+		b = heap.newClassInstance();
+		c = heap.newClassInstance();
+		d = heap.newClassInstance();
+		e = heap.newClassInstance();
+		f = heap.newArray();
+	}
 }
