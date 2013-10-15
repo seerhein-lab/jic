fea67474cb713126c86f3bdbd3f84ba4955ce4c9
Oliver Haase <haase@htwg-konstanz.de>
2013-10-15 13:06:37 +0200

    When recursion is detected, the method is considered not to throw any bugs (as opposed to redirecting analysis to the handling of virtual methods).

--- src/main/java/de/seerhein_lab/jic/analyzer/BaseVisitor.java
+++ src/main/java/de/seerhein_lab/jic/analyzer/BaseVisitor.java
@@ -251,10 +251,16 @@ public abstract class BaseVisitor extends SimpleVisitor {
 		
 		if (alreadyVisitedMethods.contains(invocation)) {
 			logger.log(Level.FINE, indentation
-					+ "Recursion found: Method already analyzed.");
+					+ "Recursion found: not analyzing again.");
 			// if already visited then do not analyze again
-			handleVirtualMethod(obj, false);
+//			handleVirtualMethod(obj, false);
+//			return;
+			
+			Slot returnValue = Slot.getDefaultSlotInstance(obj
+					.getReturnType(constantPoolGen));
+			handleSimpleInstruction(obj, returnValue);
 			return;
+			
 		}				
 		
 		Set<MethodInvocation> nowVisitedMethods = new HashSet<MethodInvocation>();
