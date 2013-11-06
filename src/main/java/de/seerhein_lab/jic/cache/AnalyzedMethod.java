a0f0e99305b85d0c0aa49ea0cf2055298f8455f4
bestehle <bestehle@htwg-konstanz.de>
2013-11-06 20:51:55 +0100

    added equals() and hashCode() to AnalyzedMethod for comparison in the Cache

--- src/main/java/de/seerhein_lab/jic/cache/AnalyzedMethod.java
+++ src/main/java/de/seerhein_lab/jic/cache/AnalyzedMethod.java
@@ -16,4 +16,47 @@ public class AnalyzedMethod {
 	public Method getMethod() {
 		return method;
 	}
+
+	@Override
+	public String toString() {
+		return clazz.getClassName() + method.getName();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((clazz == null) ? 0 : clazz.hashCode());
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof AnalyzedMethod)) {
+			return false;
+		}
+		AnalyzedMethod other = (AnalyzedMethod) obj;
+		if (clazz == null) {
+			if (other.clazz != null) {
+				return false;
+			}
+		} else if (!clazz.equals(other.clazz)) {
+			return false;
+		}
+		if (method == null) {
+			if (other.method != null) {
+				return false;
+			}
+		} else if (!method.equals(other.method)) {
+			return false;
+		}
+		return true;
+	}
 }
