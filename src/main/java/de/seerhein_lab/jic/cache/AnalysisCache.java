76951ec0fe7409fc910d1bd9105cd4fcf5c40343
Oliver Haase <haase@htwg-konstanz.de>
2013-12-06 12:57:28 +0100

    AnalyzedMethod replaced by QualifiedMethed

--- src/main/java/de/seerhein_lab/jic/cache/AnalysisCache.java
+++ src/main/java/de/seerhein_lab/jic/cache/AnalysisCache.java
@@ -3,14 +3,16 @@ package de.seerhein_lab.jic.cache;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import de.seerhein_lab.jic.analyzer.QualifiedMethod;
+
 public final class AnalysisCache {
 	public static enum Check {
 		PropCon, CtorArgsCopied;
 	}
 
-	private final Map<AnalyzedMethod, AnalysisResult> map = new ConcurrentHashMap<AnalyzedMethod, AnalysisResult>();
+	private final Map<QualifiedMethod, AnalysisResult> map = new ConcurrentHashMap<QualifiedMethod, AnalysisResult>();
 
-	public boolean contains(AnalyzedMethod method) {
+	public boolean contains(QualifiedMethod method) {
 		// if (method.getMethod().getName().equals(CONSTRUCTOR_NAME)
 		// && method.getMethod().getArgumentTypes().length == 0) {
 		return map.containsKey(method);
@@ -19,11 +21,11 @@ public final class AnalysisCache {
 
 	}
 
-	public AnalysisResult get(AnalyzedMethod method) {
+	public AnalysisResult get(QualifiedMethod method) {
 		return map.get(method);
 	}
 
-	public void add(AnalyzedMethod method, AnalysisResult result, Check check) {
+	public void add(QualifiedMethod method, AnalysisResult result, Check check) {
 		if (!map.containsKey(method)) {
 			map.put(method, result);
 		} else {
