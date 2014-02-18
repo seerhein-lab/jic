1da6446712218833d69f45980a7f2cd00eef85c8
Oliver Haase <haase@htwg-konstanz.de>
2014-02-18 11:15:50 +0100

    Findbugs warning caused by improper construction of Exception superclass suppresses.

--- src/main/java/de/seerhein_lab/jic/EmercencyBrakeException.java
+++ src/main/java/de/seerhein_lab/jic/EmercencyBrakeException.java
@@ -1,5 +1,8 @@
 package de.seerhein_lab.jic;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings(value = "PC")
 public class EmercencyBrakeException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
