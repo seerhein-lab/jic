c6c193bd06b9795a0258496ec6dc63c465a127a0
Oliver Haase <haase@htwg-konstanz.de>
2014-03-16 13:16:48 +0100

    merge operation added to AnalysisResult

--- src/main/java/de/seerhein_lab/jic/AnalysisResult.java
+++ src/main/java/de/seerhein_lab/jic/AnalysisResult.java
@@ -9,20 +9,27 @@ import edu.umd.cs.findbugs.SortedBugCollection;
 
 public class AnalysisResult {
 	private final Set<EvaluationResult> results;
-	private final Collection<BugInstance> bugs;
+	private final SortedBugCollection bugs;
 
 	public AnalysisResult(Set<EvaluationResult> results, Collection<BugInstance> bugs) {
 		this.results = results;
-		this.bugs = bugs;
+		this.bugs = new SortedBugCollection();
+		this.bugs.addAll(bugs);
 	}
 
 	public AnalysisResult() {
 		this.results = new HashSet<EvaluationResult>();
-		this.bugs = new SortedBugCollection().getCollection();
+		this.bugs = new SortedBugCollection();
+	}
+
+	public AnalysisResult merge(AnalysisResult other) {
+		this.results.addAll(other.getResults());
+		this.bugs.addAll(other.getBugs());
+		return this;
 	}
 
 	public Collection<BugInstance> getBugs() {
-		return bugs;
+		return bugs.getCollection();
 	}
 
 	public Set<EvaluationResult> getResults() {
